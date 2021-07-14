7daf497be9df9b014034ec823814f14e375f4539
AvilaJulio <34084090+AvilaJulio@users.noreply.github.com>
2021-07-14 15:10:47 -0700

    pw 8
    Falta la primera grafica del pw

---
+++ Postwork 8/server.R
@@ -0,0 +1,43 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    library(ggplot2)
+    
+    match_data <- read.csv("match.data.csv")
+    
+    #Gráfico de Histograma
+    output$plot1 <- renderPlot({
+        
+        x <- mtcars[,input$x]
+        bin <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        ggplot(mtcars, aes(x, fill = mtcars[,input$zz])) + 
+            geom_histogram( breaks = bin) +
+            labs( xlim = c(0, max(x))) + 
+            theme_light() + 
+            xlab(input$x) + ylab("Frecuencia") + 
+            facet_grid(input$zz)
+        
+        
+    })
+    
+    
+    #Data Table
+    output$data_table <- renderDataTable( {match_data}, 
+                                          options = list(aLengthMenu = c(10,15,25,50),
+                                                         iDisplayLength = 15)
+    )
+    
+
+})
---
+++ Postwork 8/ui.R
@@ -0,0 +1,116 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+        
+        dashboardPage(
+            
+            dashboardHeader(title = "PW 8 - Equipo 6"),
+            
+            dashboardSidebar(
+                
+                sidebarMenu(
+                    menuItem("Gráficos de barra", tabName = "bars", icon = icon("bar-chart")),
+                    menuItem("Goles casa-visitante", tabName = "graph", icon = icon("area-chart")),
+                    menuItem("Data Table", tabName = "data_table", icon = icon("table")),
+                    menuItem("Factores de ganancia", tabName = "img", icon = icon("file-picture-o"))
+                )
+                
+            ),
+            
+            dashboardBody(
+                
+                tabItems(
+                    
+                    # Histograma
+                    tabItem(tabName = "Dashboard",
+                            fluidRow(
+                                titlePanel("Histograma de las variables del data set mtcars"), 
+                                selectInput("x", "Seleccione el valor de X",
+                                            choices = names(mtcars)),
+                                
+                                selectInput("zz", "Selecciona la variable del grid", 
+                                            
+                                            choices = c("cyl", "vs", "gear", "carb")),
+                                box(plotOutput("plot1", height = 250)),
+                                
+                                box(
+                                    title = "Controls",
+                                    sliderInput("bins", "Number of observations:", 1, 30, 15)
+                                )
+                            )
+                    ),
+                    
+                    # PW 3
+                    tabItem(tabName = "graph",
+                            tabsetPanel(
+                                tabPanel(
+                                    "Gráfico 1",
+                                    fluidRow(
+                                        titlePanel(h3("Probabilidades marginales del número de goles que anota el equipo de casa")),
+                                        img(src ="1.png", width = 450, height = 450)
+                                    )
+                                ),
+                                tabPanel(
+                                    "Gráfico 2",
+                                    fluidRow(
+                                        titlePanel(h3("Prob. marg. del número de goles que anota el equipo de visitante")),
+                                        img(src ="2.png", width = 450, height = 450)
+                                    )
+                                ),
+                                tabPanel(
+                                    "Gráfico 3",
+                                    fluidRow(
+                                        titlePanel(h3("HeatMap para las probabilidades conjuntas")),
+                                        img(src ="3.png", width = 450, height = 450)
+                                    )
+                                )
+                            )
+                    ),
+                    
+                    
+                    # data table
+                    tabItem(tabName = "data_table",
+                            fluidRow(        
+                                titlePanel(h3("Data Table")),
+                                dataTableOutput ("data_table")
+                            )
+                    ), 
+                    
+                    tabItem(tabName = "img",
+                            tabsetPanel(
+                                tabPanel(
+                                    "Máx.",
+                                    fluidRow(
+                                        titlePanel(h3("Factores de ganancia máximo")),
+                                        img(src ="momio_max.png", height = 450)
+                                        
+                                    )
+                            ),
+                                tabPanel(
+                                    "Avg.",
+                                    fluidRow(
+                                        titlePanel(h3("Factores de ganancia promedio")),
+                                        img(src ="momio_avg.png", height = 450)
+                                )
+                            )
+                        )
+                    )
+                    
+                )
+            )
+        )
+    )
+    
+)
